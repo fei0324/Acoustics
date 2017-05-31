b6a53e702a2004a92e0527bbc932188bd135f59b
fei0324 <fl7834a@student.american.edu>
2017-05-31 19:22:15 -0400

    Added subdivision points

--- BarycentricSubdivision.py
+++ BarycentricSubdivision.py
@@ -6,8 +6,8 @@ def barycentric(pt1, pt2, pt3, n):
 	"""
 	Convert STL data into unit normal vectors and points on the triangles
 
-	Input: Triangles (three 3d points) (3 numpy arrays)?
-		   a number of subdivisions to perform???
+	Input: Triangles (three 3d points) (3 numpy arrays)
+		   a number of subdivisions to perform
 	Output: Points on those triangles
 			unit normal vectors points
 	"""
@@ -19,9 +19,22 @@ def barycentric(pt1, pt2, pt3, n):
 	#print(normalVector)
 	unitNorVec = normalVector/np.linalg.norm(normalVector)
 	#print(unitNorVec)
-	return unitNorVec
 
-pt1 = np.array([1,1,1])
-pt2 = np.array([-1,1,0])
-pt3 = np.array([2,0,3])
-barycentric(pt1, pt2, pt3, 0)
+	# find subdivision points in barycentric coordinates
+	points = [] #points includes the vertices of the original triangle
+	vectorU = pt2 - pt1
+	vectorV = pt3 - pt1
+	#print("U = " + str(vectorU))
+	#print("V = " + str(vectorV))
+	for i in range(0, 2**n+1):
+		for j in range(0, 2**n+1-i):
+			points.append(pt1 + (i/2.**n)*vectorU + (j/2.**n)*vectorV)
+			#print(points)
+	#print(len(points))
+	
+	return unitNorVec, points
+
+pt1 = np.array([1,0,0])
+pt2 = np.array([0,1,0])
+pt3 = np.array([0,0,1])
+barycentric(pt1, pt2, pt3, 2)
