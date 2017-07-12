39ef862b3bbd74b0ca0567933b1d9ea9363c20e2
fei0324 <fl7834a@student.american.edu>
2017-07-12 01:13:55 -0400

    triangle test + reorganize

--- barycentric_test.py
+++ barycentric_test.py
@@ -6,17 +6,19 @@ import subdivision
 
 class BarycentricTestCase(unittest.TestCase):
 	def setUp(self):
+
 		self.pt1 = np.array([0,0,1])
 		self.pt2 = np.array([0,1,0])
 		self.pt3 = np.array([1,0,0])
-		self.n = 4
+		self.n = 3
+		self.points, self.triangleSet, self.unitNorVec = subdivision.barycentric(self.pt1,self.pt2,self.pt3,self.n)
 
 	def test_type(self):
-		points, triangleSet, unitNorVec = subdivision.barycentric(self.pt1, self.pt2, self.pt3, 1)
+
 		print("Testing type of 'points'.")
-		self.assertTrue(type(points) == list, msg="Type of 'points' is incorrect.")
+		self.assertTrue(type(self.points) == list, msg="Type of 'points' is incorrect.")
 		print("Testing type of unit normal vector.")
-		self.assertTrue(type(unitNorVec) == np.ndarray, msg="Type of unit normal vector is incorrect.")
+		self.assertTrue(type(self.unitNorVec) == np.ndarray, msg="Type of unit normal vector is incorrect.")
 
 
 	def test_sizeOfPoints(self):
@@ -30,15 +32,47 @@ class BarycentricTestCase(unittest.TestCase):
 			#verify the length of subdivision point list
 			self.assertEqual(len(points), numbOfPts, msg="Incorrect number of subdivision points")
 
+	def test_pointInPoints(self):
+
+		print("Testing individual points in the points list.")
+		#verify x of the 5th point (index 4)
+		self.assertTrue(abs(self.points[4][0]-0.5)<1e-5, msg="Incorrect subdivision.")
+		#verify z of the 20th point (index 19)
+		self.assertTrue(abs(self.points[19][2]-0.5)<1e-5, msg="Incorrect subdivision.")
+		#verify y of the 31st point (index 30)
+		self.assertTrue(abs(self.points[30][1]-0.5)<1e-5, msg="Incorrect subdivision.")
+
 	def test_unitNorVec(self):
 
-		points, triangleSet, unitNorVec = subdivision.barycentric(self.pt1, self.pt2, self.pt3, 1)
 		#verify normal vector
 		print("Testing unit normal vectors.")
-		self.assertTrue(abs(unitNorVec[0]-(-1/math.sqrt(3)))<1e-5, msg="Unit normal vector first component incorrect.")
+		self.assertTrue(abs(self.unitNorVec[0]-(-1/math.sqrt(3)))<1e-5, msg="Unit normal vector first component incorrect.")
 
-		#verify second component of points is correct
-		#self.assertTrue(abs(points[1][0]-0.5)<1e-5, msg="Incorrect subdivision.")
+	def test_sizeOfTriangleSet(self):
+		
+		print("Testing size of triangle set.")
+		numOfTriangles = 0
+		for i in range(1, 2**(self.n+1), 2):
+			numOfTriangles += i
+		self.assertEqual(len(self.triangleSet), numOfTriangles, msg="Incorrect size for triangle set.")
+
+	def test_pointInTriangleSet(self):
+
+		print("Testing individual points in the triangle set.")
+		#verify x of the second point in the 12th triangle (index 11)
+		self.assertTrue(abs(self.triangleSet[11][1][0]-0.5)<1e-5, msg="Incorrect triangle triples.")
+		#verify x of the third point in the 49th triangle (index 48)
+		self.assertTrue(abs(self.triangleSet[48][2][0]-0.)<1e-5, msg="Incorrect triangle triples.")
+
+	def test_triangleArea(self):
+		
+		triangleAreaSet = subdivision.triangleArea(self.triangleSet)
+		print("Testing triangle area.")
+		v1 = self.pt2 - self.pt1
+		v2 = self.pt3 - self.pt1
+		areaOftriangle = np.linalg.norm(np.cross(v1,v2))/2
+		self.assertTrue(abs(areaOftriangle/len(self.triangleSet)-triangleAreaSet[0])<1e-5, msg="Incorrect triangle area.")
+
 
 if __name__ == '__main__':
 	unittest.main()
--- subdivision.py
+++ subdivision.py
@@ -1,8 +1,6 @@
 import math
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from stl import mesh
+
 
 def barycentric(pt1, pt2, pt3, n):
 
@@ -74,62 +72,7 @@ def triangleArea(triangleSet):
 	for i in range(len(triangleSet)):
 		v1 = triangleSet[i][1] - triangleSet[i][0]
 		v2 = triangleSet[i][2] - triangleSet[i][0]
-		area = np.cross(v1, v2)/2
+		area = np.linalg.norm(np.cross(v1, v2))/2
 		triangleAreaSet.append(area)
 
 	return triangleAreaSet
-
-def plotP_V(points, unitNorVec, ax):
-
-	"""
-	Plot all the subdivision points and the unit normal vectors
-
-	Input: points and unitNorVec returned from the barycentric function
-
-	Output: A 3d plot of points and vectors
-	"""
-
-	xs = []
-	ys = []
-	zs = []
-
-	u = unitNorVec[0]
-	v = unitNorVec[1]
-	w = unitNorVec[2]
-
-	for i in range(0, len(points)):
-		xs.append(points[i][0])
-		ys.append(points[i][1])
-		zs.append(points[i][2])
-
-	ax.plot(xs, ys, zs, 'o')
-	#ax.quiver(xs, ys, zs, u, v, w, length=0.2, normalize=True)
-
-fig = plt.figure(figsize=plt.figaspect(0.5))
-fig.suptitle("Test for Subdivision", fontsize = 18)
-ax1 = fig.add_subplot(1,2,1,projection='3d')
-ax1.set_title("Sphere STL Subdivision")
-ax2 = fig.add_subplot(1,2,2,projection='3d')
-ax2.set_title("Cylinder STL Subdivision")
-
-sphere_mesh = mesh.Mesh.from_file('sphere.stl')
-cylinder_mesh = mesh.Mesh.from_file('cylinder.stl')
-plt.hold(True)
-
-for i in range(len(sphere_mesh.vectors)):
-	points, triangleSet, unitNorVec = barycentric(sphere_mesh.vectors[i][0], sphere_mesh.vectors[i][1], sphere_mesh.vectors[i][2],3)
-	triangleAreaSet = triangleArea(triangleSet)
-	plotP_V(points, unitNorVec, ax1)
-
-for j in range(len(cylinder_mesh.vectors)):
-	points1, triangleSet1, unitNorVec1 = barycentric(cylinder_mesh.vectors[j][0], cylinder_mesh.vectors[j][1], cylinder_mesh.vectors[j][2],4)
-	triangleAreaSet1 = triangleArea(triangleSet1)
-	plotP_V(points1, unitNorVec1, ax2)
-
-plt.show()
-print("Sphere points = " + str(len(points)))
-print("Sphere triangles = " + str(len(triangleSet)))
-print("Sphere triangle areas = " + str(len(triangleAreaSet)))
-print("Cylinder points = " + str(len(points1)))
-print("Cylinder triangles = " + str(len(triangleSet1)))
-print("Cylinder triangle areas = " + str(len(triangleAreaSet1)))
