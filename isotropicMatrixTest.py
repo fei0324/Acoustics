ce393bacf7ef009d38e9d760775fcbc502e64bd5
fei0324 <fl7834a@student.american.edu>
2017-07-23 22:55:13 -0400

    isotropic test fixed + LSolver finished (needs testing)

--- isotropicMatrixTest.py
+++ isotropicMatrixTest.py
@@ -16,19 +16,19 @@ class isoMatrixFunctionTestCase(unittest.TestCase):
 		self.source3 = np.array([[0,0,0]])
 		self.sample3 = np.array([[0,1,0],[1,0,0],[1,1,0]])
 
-#tests for column values 
+	#tests for column values 
 	def test_colums(self):
 		M = isoMat.isotropicMatrix(1,self.source1,self.sample1)
 		print("testing colums")
 		for i in range(0,len(M)):
 			if i < len(M)-1:
-				self.assertEqual(M[i][0], M[i+1][0], msg="colum test failed.")
+				self.assertEqual(M[i][0], M[i+1][0], msg="column test failed.")
 
 	# test for row values
 	def test_row(self):
 		M = isoMat.isotropicMatrix(1,self.source2,self.sample2)  
 		print("testing rows values")
-		self.assertEqual(M[1][1], M[2][1], msg="row test failed")	
+		self.assertEqual(M[1][1], M[2][1], msg="row test failed.")	
 
 	# test for specific values
 	def test_specific(self):
@@ -37,7 +37,7 @@ class isoMatrixFunctionTestCase(unittest.TestCase):
 		h = (math.cos(2*math.pi*math.sqrt(2)))/(4*math.pi*math.sqrt(2))
 		l = (math.sin(2*math.pi*math.sqrt(2)))/(4*math.pi*math.sqrt(2))*(-1) 
 		print("testing specific values")
-		self.assertTrue(abs(M[2][0]-complex(h,l))<(1e-5+1e-5j), msg="specific test failed")
+		self.assertTrue(abs(M[2][0]-complex(h,l))<(1e-5+1e-5j), msg="specific test failed.")
 
 if __name__ == '__main__':
 	unittest.main()
--- youngsLSolver.py
+++ youngsLSolver.py
@@ -2,6 +2,27 @@ import math
 import numpy as np
 from stl import mesh
 
+def rotationMat(n):
+
+	"""
+	Create a transformation matrix rotate the triangle plane to the xy plane
+	Preparation for checking if point is inside of a triangle
+	Step after translation t: (x,y,z) -> (x,y,z-d/c)
+	Input: n = the normal vector of the triangle plane
+	Output: rotation matrix
+	"""
+	cosAngle = n[2]/np.linalg.norm(n)
+	sinAngle = np.sin(np.arccos(cosAngle))
+
+	u = np.array([n[1],-1*n[0],0])/np.linalg.norm(n)
+	rotMat = np.zeros((3,3))
+	rotMat[0] = [cosAngle+u[0]**2*(1-cosAngle),u[0]*u[1]*(1-cosAngle),u[1]*sinAngle]
+	rotMat[1] = [u[0]*u[1]*(1-cosAngle),cosAngle+u[1]**2*(1-cosAngle),-1*u[0]*sinAngle]
+	rotMat[2] = [-1*u[1]*sinAngle,u[0]*sinAngle,cosAngle]
+
+	return rotMat
+
+
 def LSolver(triangles,triNormVecs,forceVecs):
 
 	"""
@@ -31,10 +52,27 @@ def LSolver(triangles,triNormVecs,forceVecs):
 				t = numer/denom
 				intersectPoint = p[i] + t*forceVecs[i]
 
+				# Transform the intersection point and vertices of the triangle
+				rotMat = rotationMat(triNormVecs[j])
+				d = np.dot(triNormVecs[j], p[j])
+				pt1 = triangles[j][0]
+				pt2 = triangles[j][1]
+				pt3 = triangles[j][2] + d/triNormVecs[j][2]
+				intersectPoint[2] = intersectPoint[2] + d/triNormVecs[j][2]
+
+				newPt1 = np.dot(rotMat,pt1)
+				newPt2 = np.dot(rotMat,pt2)
+				newPt3 - np.dot(rotMat,pt3)
+				newIntersect = np.dot(rotMat,intersectPoint)
+
 				# Verify if the point is inside of the triangle
-				
+				a = [[(newPt2-newPt1)[0],(newPt3-newPt1)[0]],[(newPt2-newPt1)[1],(newPt3-newPt1)[1]]]
+				b = [(newIntersect-newPt1)[0],(newIntersect-newPt1)[1]]
+				x = np.linalg.solve(a,b)
 
-
+				if 0<=x[0]<=1 and 0<=x[1]<=1:
+					L = t*forceVecs[i]
+	return L
 
 
 # read an stl file
@@ -42,4 +80,4 @@ sphere_mesh = mesh.Mesh.from_file('sphere.stl')
 triNormVecs = sphere_mesh.normals
 triangles = sphere_mesh.vectors
 
-LSolver(triangles, triNormVecs, [0,0,0])
+#LSolver(triangles, triNormVecs, [[0,0,0],[1,1,1]])
